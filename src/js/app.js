d4175bde0761756d185d0a232dcaf8699312fdac
creat1ve54 <100487936+creat1ve54@users.noreply.github.com>
2023-06-19 20:29:11 +0300

    first

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+import * as flsFunctions from "./modules/functions.js";
+import "./swiper.js";
+import IMask from 'imask';
+import { Fancybox, Carousel, Panzoom } from "@fancyapps/ui";
+import Schema from 'validate'
+
+//маска
+// IMask(document.getElementById('phone'), {
+//     mask: '+{7}(000)000-00-00'
+// });
+
+
+// Fancybox.bind('[data-fancybox="gallery"]', {
+//     // Your custom options
+// });
+
+flsFunctions.isWebp();
---
+++ src/js/swiper.js
@@ -0,0 +1,8 @@
+
+let mySwiper = document.querySelector('.mySwiper')
+
+let swiper = new Swiper(mySwiper, {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
